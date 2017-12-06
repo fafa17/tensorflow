e7a9ab58dc3243e61ad61aeef7a348ba89565e14
cyliu <cyliu@cse.cuhk.edu.hk>
2017-12-06 12:06:18 +0800

    include python testing file

---
+++ tensorflow/python/selftf/test_master_stub.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+import grpc
+from tensorflow.core.protobuf import master_pb2
+
+
+class Stub(object):
+
+  def __init__(self, channel):
+    """
+    :param grpc.Channel channel:
+    """
+
+    self.Reconfig = channel.unary_unary(
+        '/tensorflow.MasterService/Reconfig',
+        request_serializer=master_pb2.ReconfigRequest.SerializeToString,
+        response_deserializer=master_pb2.ReconfigResponse.FromString,
+    )
+
+
+def run():
+  channel = grpc.insecure_channel('localhost:2222')
+  stub = Stub(channel)
+  print("-------------- Reconfig --------------")
+  stub.Reconfig(master_pb2.ReconfigRequest())
+
+run()
